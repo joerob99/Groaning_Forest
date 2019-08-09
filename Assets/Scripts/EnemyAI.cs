0fe801eb4d91415a6d24e2752d29feb1a433eeed
joerob99 <josephnrobinson1999@gmail.com>
2019-08-09 16:24:12 -0400

    Enemy now continues to chase when provoked and attacks when close

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -9,6 +10,7 @@ public class EnemyAI : MonoBehaviour
     [SerializeField] float chaseRange = 8f;
     NavMeshAgent navMeshAgent;
     float distToTarget = Mathf.Infinity;
+    bool isProvoked = false;
 
     void Start()
     {
@@ -18,11 +20,31 @@ public class EnemyAI : MonoBehaviour
     void Update()
     {
         distToTarget = Vector3.Distance(target.position, transform.position);
-        if (distToTarget <= chaseRange)
+        if (isProvoked)
         {
+            EngageTarget();
+        }
+        else if (distToTarget <= chaseRange)
+        {
+            isProvoked = true;
             navMeshAgent.SetDestination(target.position);
         }
     }
+
+    private void EngageTarget()
+    {
+        if (distToTarget > navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+        if (distToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+
+    private void ChaseTarget() { navMeshAgent.SetDestination(target.position); }
+    private void AttackTarget() { print("Gotcha"); }
 
     void OnDrawGizmosSelected()
     {
