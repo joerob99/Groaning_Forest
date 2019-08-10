c66e0ab269d30b25783cc0ae9afd64fc56321b72
joerob99 <josephnrobinson1999@gmail.com>
2019-08-10 16:58:13 -0400

    Added the player's health

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] float playerHealth = 100f;
+
+    public void LoseHealth(float damage)
+    {
+        playerHealth -= damage;
+        if (playerHealth <= 0f) { print("Deaded..."); }
+    }
+}
