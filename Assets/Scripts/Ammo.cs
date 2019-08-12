8f1288d1578fd3d5e6bc9aa60949ec499fe5df46
joerob99 <josephnrobinson1999@gmail.com>
2019-08-11 22:02:16 -0400

    Ammo pickups with visuals added as well as new bullet duplicates without rigibodies

--- Assets/Scripts/Ammo.cs
+++ Assets/Scripts/Ammo.cs
@@ -17,6 +17,8 @@ public class Ammo : MonoBehaviour
 
     public void ReduceCurrentAmmo(AmmoType ammoType) { (GetAmmoSlot(ammoType).ammoAmount)--; }
 
+    public void IncreaseCurrentAmmo(AmmoType ammoType, int ammoAmount) { (GetAmmoSlot(ammoType).ammoAmount) += ammoAmount; }
+
     private AmmoSlot GetAmmoSlot(AmmoType ammoType)
     {
         foreach(AmmoSlot slot in ammoSlots)
