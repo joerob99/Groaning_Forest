e1573fb35e98f7d1b653afcf506c0787a7b28fbe
joerob99 <josephnrobinson1999@gmail.com>
2019-08-22 18:30:53 -0400

    Added a more comprehensive main menu interface with different buttons for controls and credits

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] Canvas controlScreen;
+    [SerializeField] Canvas creditScreen;
+    [SerializeField] Canvas mainMenu;
+
+    void Start()
+    {
+        controlScreen.enabled = false;
+        creditScreen.enabled = false;
+    }
+
+    public void ShowControls()
+    {
+        mainMenu.enabled = false;
+        controlScreen.enabled = true;
+    }
+
+    public void ShowCredits()
+    {
+        mainMenu.enabled = false;
+        creditScreen.enabled = true;
+    }
+
+    public void ShowMainMenu()
+    {
+        controlScreen.enabled = false;
+        creditScreen.enabled = false;
+        mainMenu.enabled = true;
+    }
+}
