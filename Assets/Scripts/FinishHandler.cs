1cb77f190e48d09759ebb5f7b36db2b59843da92
joerob99 <josephnrobinson1999@gmail.com>
2019-08-28 16:37:32 -0400

    Added a finishing screen and a script on the player to enable it, not yet tested so not sure if it works properly yet.

---
+++ Assets/Scripts/FinishHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishHandler : MonoBehaviour
+{
+    [SerializeField] Canvas winScreen;
+
+    void Start()
+    {
+        winScreen.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Finish") { HandleFinish(); }
+    }
+
+    private void HandleFinish()
+    {
+        winScreen.enabled = true;
+        Time.timeScale = 0;
+        FindObjectOfType<WeaponSwitcher>().enabled = false;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
