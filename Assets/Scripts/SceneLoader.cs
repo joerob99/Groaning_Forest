83d2ed6d3db84d5e3243ecded516d67c874428e5
joerob99 <josephnrobinson1999@gmail.com>
2019-09-08 17:01:48 -0400

    Fixed a bug where the space bar could toggle pause and perform other scene-based actions

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -9,24 +9,27 @@ public class SceneLoader : MonoBehaviour
 
     public void LoadGame()
     {
+        if (Input.GetKeyDown(KeyCode.Space)) { return; } // Fixes a bug with spacebar
         ReloadGame();
     }
 
     public void ReloadGame()
     {
+        if (Input.GetKeyDown(KeyCode.Space)) { return; } // Fixes a bug with spacebar
         SceneManager.LoadScene(1);
         Time.timeScale = 1;
     }
 
     public void QuitToMainMenu()
     {
+        if (Input.GetKeyDown(KeyCode.Space)) { return; } // Fixes a bug with spacebar
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
         SceneManager.LoadScene(0);
         Time.timeScale = 0;
     }
 
-    public void TogglePauseOnAndOff() { pause.TogglePause(); }
+    public void TogglePauseOnAndOff() { if (Input.GetKeyDown(KeyCode.Space)) { return; } pause.TogglePause(); }
 
     public void QuitGame()
     {
